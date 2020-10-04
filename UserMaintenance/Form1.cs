8c9051b113531aee74fce1962ac24afe113b5694
Áron Pintér <aron.pinter@stud.uni-corvinus.hu>
2020-10-04 10:40:38 +0200

    feature

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -20,8 +20,7 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            
             button1.Text = Resource1.Add;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -31,7 +30,7 @@ namespace UserMaintenance
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var u = new User() { LastName = textBox1.Text, FirstName = textBox2.Text };
+            var u = new User() { FullName = "textBox1.Text + textBox2.Text "};
             users.Add(u);
 
         }
