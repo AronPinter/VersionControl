01fbe666ba89a68f72e335c451471a6461795d06
Áron Pintér <aron.pinter@stud.uni-corvinus.hu>
2020-10-11 10:10:41 +0200

    create excel

--- VIF5G3_week04/Form1.cs
+++ VIF5G3_week04/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace VIF5G3_week04
 {
@@ -16,15 +17,42 @@ namespace VIF5G3_week04
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
-  
+        Excel.Application x1App;
+        Excel.Workbook x1WB;
+        Excel.Worksheet x1Sheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+        }
+        private void CreateExcel() 
+        {
+            try
+            {
+                x1App = new Excel.Application();
+                x1WB = x1App.Workbooks.Add(Missing.Value);
+                x1Sheet = x1WB.ActiveSheet;
+
+                CreateTable();
+
+                x1App.Visible = True;
+                x1App.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 
