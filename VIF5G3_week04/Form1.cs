1063ecf697717a1bd7152be1d7162a6ce9c4f85c
Áron Pintér <aron.pinter@stud.uni-corvinus.hu>
2020-10-11 11:07:51 +0200

    export

--- VIF5G3_week04/Form1.cs
+++ VIF5G3_week04/Form1.cs
@@ -17,7 +17,8 @@ namespace VIF5G3_week04
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
-        Excel.Application x1App;
+
+        Excel.Application x1App ;
         Excel.Workbook x1WB;
         Excel.Worksheet x1Sheet;
         public Form1()
@@ -40,7 +41,7 @@ namespace VIF5G3_week04
 
                 CreateTable();
 
-                x1App.Visible = True;
+                x1App.Visible = true;
                 x1App.UserControl = true;
             }
             catch (Exception ex)
@@ -48,11 +49,72 @@ namespace VIF5G3_week04
                 string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
                 MessageBox.Show(errMsg, "Error");
 
-                xlWB.Close(false, Type.Missing, Type.Missing);
-                xlApp.Quit();
-                xlWB = null;
-                xlApp = null;
+                x1WB.Close(false, Type.Missing, Type.Missing);
+                x1App.Quit();
+                x1WB = null;
+                x1App = null;
             }
+        }
+        private void CreateTable()
+        {
+            string[] headers = new string[] 
+            {"Kód",
+            "Eladó",
+            "Oldal",
+            "Kerület",
+            "Lift",
+            "Szobák száma",
+            "Alapterület (m2)",
+            "Ár (mFt)",
+            "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                x1Sheet.Cells[1, i+1] = headers[1];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator)
+                {
+                    values[counter, 4] ="van";
+                }
+                else
+                {
+                    values[counter, 4] = "nincs";
+                }
+                
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+            x1Sheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 
