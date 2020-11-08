f0158edb5590799ba26e7699feef527850f25f4e
Áron Pintér <aron.pinter@stud.uni-corvinus.hu>
2020-11-08 12:39:13 +0100

    666

--- week07/Form1.cs
+++ week07/Form1.cs
@@ -26,7 +26,8 @@ namespace week07
                
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    
+                    SimStep(year, Population[i]);
+
                 }
 
                 int nbrOfMales = (from x in Population
@@ -111,6 +112,36 @@ namespace week07
         }
 
         Random rng = new Random(1234);
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.Probability).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.Probability).FirstOrDefault();
+                
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
 
     }
 }
